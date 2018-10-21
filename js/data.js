6aed161e3ef31ba78aa956fb7e021f5ab11b5e32
carolinaalmeida1 <carolina.almeida1@outlook.com>
2018-10-21 11:02:14 -0300

    first version mvp

---
+++ js/data.js
@@ -0,0 +1,56 @@
+const nasaData = [
+  {
+    "name": "The California Current System",
+    "image": "assets/images/GSFC_20171208_Archive_e000413_medium.jpg",
+    "description": "This February 8, 2016 composite image reveals the complex distribution of phytoplankton in one of Earth's eastern boundary upwelling systems — the California Current. Recent work suggests that our warming climate my be increasing the intensity of upwelling in such regions with possible repercussions for the species that comprise those ecosystems. NASA's OceanColor Web is supported by the Ocean Biology Processing Group (OBPG) at NASA's Goddard Space Flight Center. Our responsibilities include the collection, processing, calibration, validation, archive and distribution of ocean-related products from a large number of operational, satellite-based remote-sensing missions providing ocean color, sea surface temperature and sea surface salinity data to the international research community since 1996.",
+    "type": "ecosystem"
+  },
+  {
+    "name": "Shaded Relief of Rio Sao Francisco, Brazil",
+    "image": "assets/images/PIA02700_medium.jpg",
+    "description": "This topographic image acquired by SRTM shows an area south of the Sao Francisco River in Brazil. The scrub forest terrain shows relief of about 400 meters (1300 feet). Areas such as these are difficult to map by traditional methods because of frequent cloud cover and local inaccessibility. This region has little topographic relief, but even subtle changes in topography have far-reaching effects on regional ecosystems. The image covers an area of 57 km x 79 km and represents one quarter of the 225 km SRTM swath. Colors range from dark blue at water level to white and brown at hill tops. The terrain features that are clearly visible in this image include tributaries of the Sao Francisco, the dark-blue branch-like features visible from top right to bottom left, and on the left edge of the image, and hills rising up from the valley floor. The San Francisco River is a major source of water for irrigation and hydroelectric power.",
+    "type": "ecosystem"
+  },
+  {
+    "name": "Tropical Cyclone Glenda in the Indian Ocean",
+    "image": "assets/images/GSFC_20171208_Archive_e000780_large.jpg",
+    "description": "Tropical Cyclone Glenda took a five day tour of the Southern Indian Ocean in late February, 2015. The storm formed from a low pressure system, System 90S on February 24, when maximum sustained winds reached 40 mph (64 km/h). The Moderate Resolution Imaging Spectroradiometer (MODIS) aboard NASA’s Aqua satellite captured this true-color image of Tropical Storm Glenda on February 25 at 08:55 UTC (3:55 a.m. EST). At that time bands of thunderstorms wrapped into the low-level center of circulation. An eye was beginning to form. At 0900 UTC (4 a.m. EST) on February 25, Glenda's maximum sustained winds were near 63.2 mph (102 km/h). It was centered near 17.6 south latitude and 69.1 east longitude, about 760 miles (1,224 km) south-southwest of Diego Garcia. Glenda was moving to the west-southwest at 8 mph (13 km/h). At that time, the Joint Typhoon Warning Center expect Glenda to strengthen to near 109 mph (176 km/h) before beginning to weaken.",
+    "type": "ocean"
+  },
+  {
+    "name": "Pacific Ocean in Holding Pattern for El Niño",
+    "image": "assets/images/PIA03850_orig.jpg",
+    "description": "The Pacific Ocean doesnt show signs of anything that looks like the whopper El Niño of 1997-1998, according to the latest information from NASA U.S.-French ocean-observing satellite Topex/Poseidon.",
+    "type": "ocean"
+  },
+  {
+    "name": "Earth Science",
+    "image": "assets/images/0202795_medium.jpg",
+    "description": "This image depicts a full view of the Earth, taken by the Geostationary Operational Environment Satellite (GOES-8). The red and green charnels represent visible data, while the blue channel represents inverted 11 micron infrared data. The north and south poles were not actually observed by GOES-8. To produce this image, poles were taken from a GOES-7 image. Owned and operated by the National Oceanic and Atmospheric Administration (NOAA), GOES satellites provide the kind of continuous monitoring necessary for intensive data analysis. They circle the Earth in a geosynchronous orbit, which means they orbit the equatorial plane of the Earth at a speed matching the Earth's rotation. ",
+    "type": "earth"
+  },
+  {
+    "name": "Earth Observation",
+    "image": "assets/images/iss040e064832_large.jpg",
+    "description": "Earth observation taken by the Expedition 40 crew aboard the International Space Station (ISS). Folder labels this as: TL Aurora w New Zealand at end.",
+    "type": "earth"
+  },
+  {
+    "name": "Whirlpool Galaxy",
+    "image": "assets/images/PIA04230_orig.jpg",
+    "description": "The image from NASA Hubble Telescope shows spiral arms and dust clouds in the nearby Whirlpool galaxy. Visible starlight and light from the emission of glowing hydrogen is seen, which is associated with the most luminous young stars in the spiral arms.",
+    "type": "galaxy"
+  },
+  {
+    "name": "Andromeda Galaxy",
+    "image": "assets/images/PIA04921_medium.jpg",
+    "description": "This image is from NASA Galaxy Evolution Explorer is an observation of the large galaxy in Andromeda, Messier 31. The Andromeda galaxy is the most massive in the local group of galaxies that includes our Milky Way.",
+    "type": "galaxy"
+  },
+  {
+    "name": "Full Moon",
+    "image": "assets/images/GSFC_20171208_Archive_e000868_large.jpg",
+    "description": "Full Moon. Rises at sunset, high in the sky around midnight. Visible all night. NASA's Lunar Reconnaissance Orbiter (LRO) has been in orbit around the Moon since the summer of 2009. Its laser altimeter (LOLA) and camera (LROC) are recording the rugged, airless lunar terrain in exceptional detail, making it possible to visualize the Moon with unprecedented fidelity. This is especially evident in the long shadows cast near the terminator, or day-night line.",
+    "type": "moon"
+  }
+]
