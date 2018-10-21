fc5f2465f5149a27b1f37f59c2fb2eb77f897162
carolinaalmeida1 <carolina.almeida1@outlook.com>
2018-10-21 13:24:23 -0300

    first page ok

--- js/app.js
+++ js/app.js
@@ -2,33 +2,14 @@ $(document).ready(function () {
 
   function insert(nasaData) {
     nasaData.forEach((boxNasa, index) => {
-      $('.content').append('<div id="' + index + '"class="nasadata"' + index + '"> <h1 class="nasadata-name">' + boxNasa.name + '</h1> <img src="' + boxNasa.image + '"width="30%">');
-      $('#' + index).click(function () {
-        $('.content').append('<div class="modal" id="modal' + index + '" tabindex="-1" role="dialog"> <div class="modal-dialog" role="document"> <div class="modal-content"> <div class="modal-header"> <h5 class="modal-title">' + name + '</h5> <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button> </div> <div class="modal-body"><img src="' + boxNasa.image + '" width="200px" height="200px"><p>' + boxNasa.description + '</p> <strong>' + boxNasa.type + '</strong> </div> <div class="modal-footer"> <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> </div> </div> </div> </div>');
-      });
+      $(".all-boxes").append('<div id="' + index + '"class="nasadata"' + index + '"> <h1 class="nasadata-name">' + boxNasa.name + '</h1> <img src="' + boxNasa.image + '"width="30%">');
     });
   }
   insert(nasaData);
 
-  // $('.type').each(function () {
-  //   $(this).on('click', function () {
-  //     searchByType($(this).val());
-  //   });
-  // })
-
-  // function searchByType(el) {
-  //   $('strong').each(function () {
-  //     if ($(this).text() !== el) {
-  //       $(this).parent().fadeOut('slow');
-  //     } if ($(this).text() === el) {
-  //       $(this).parent().fadeIn('slow');
-  //     }
-  //   })
-  // }
-
-  // $('.input').on('keyup', function () {
-  //   $('.restaurant-name').parent().stop().fadeOut('slow');
-  //   $('.restaurant-name').each(search);
-  // });
+  $(".ocean-page").click(function () {
+    event.preventDefault();
+    window.location = "ocean.html"
+  });
 
 });
--- js/data.js
+++ js/data.js
@@ -17,40 +17,40 @@ const nasaData = [
     "description": "Tropical Cyclone Glenda took a five day tour of the Southern Indian Ocean in late February, 2015. The storm formed from a low pressure system, System 90S on February 24, when maximum sustained winds reached 40 mph (64 km/h). The Moderate Resolution Imaging Spectroradiometer (MODIS) aboard NASA’s Aqua satellite captured this true-color image of Tropical Storm Glenda on February 25 at 08:55 UTC (3:55 a.m. EST). At that time bands of thunderstorms wrapped into the low-level center of circulation. An eye was beginning to form. At 0900 UTC (4 a.m. EST) on February 25, Glenda's maximum sustained winds were near 63.2 mph (102 km/h). It was centered near 17.6 south latitude and 69.1 east longitude, about 760 miles (1,224 km) south-southwest of Diego Garcia. Glenda was moving to the west-southwest at 8 mph (13 km/h). At that time, the Joint Typhoon Warning Center expect Glenda to strengthen to near 109 mph (176 km/h) before beginning to weaken.",
     "type": "ocean"
   },
-  {
-    "name": "Pacific Ocean in Holding Pattern for El Niño",
-    "image": "assets/images/PIA03850_orig.jpg",
-    "description": "The Pacific Ocean doesnt show signs of anything that looks like the whopper El Niño of 1997-1998, according to the latest information from NASA U.S.-French ocean-observing satellite Topex/Poseidon.",
-    "type": "ocean"
-  },
-  {
-    "name": "Earth Science",
-    "image": "assets/images/0202795_medium.jpg",
-    "description": "This image depicts a full view of the Earth, taken by the Geostationary Operational Environment Satellite (GOES-8). The red and green charnels represent visible data, while the blue channel represents inverted 11 micron infrared data. The north and south poles were not actually observed by GOES-8. To produce this image, poles were taken from a GOES-7 image. Owned and operated by the National Oceanic and Atmospheric Administration (NOAA), GOES satellites provide the kind of continuous monitoring necessary for intensive data analysis. They circle the Earth in a geosynchronous orbit, which means they orbit the equatorial plane of the Earth at a speed matching the Earth's rotation. ",
-    "type": "earth"
-  },
-  {
-    "name": "Earth Observation",
-    "image": "assets/images/iss040e064832_large.jpg",
-    "description": "Earth observation taken by the Expedition 40 crew aboard the International Space Station (ISS). Folder labels this as: TL Aurora w New Zealand at end.",
-    "type": "earth"
-  },
-  {
-    "name": "Whirlpool Galaxy",
-    "image": "assets/images/PIA04230_orig.jpg",
-    "description": "The image from NASA Hubble Telescope shows spiral arms and dust clouds in the nearby Whirlpool galaxy. Visible starlight and light from the emission of glowing hydrogen is seen, which is associated with the most luminous young stars in the spiral arms.",
-    "type": "galaxy"
-  },
-  {
-    "name": "Andromeda Galaxy",
-    "image": "assets/images/PIA04921_medium.jpg",
-    "description": "This image is from NASA Galaxy Evolution Explorer is an observation of the large galaxy in Andromeda, Messier 31. The Andromeda galaxy is the most massive in the local group of galaxies that includes our Milky Way.",
-    "type": "galaxy"
-  },
-  {
-    "name": "Full Moon",
-    "image": "assets/images/GSFC_20171208_Archive_e000868_large.jpg",
-    "description": "Full Moon. Rises at sunset, high in the sky around midnight. Visible all night. NASA's Lunar Reconnaissance Orbiter (LRO) has been in orbit around the Moon since the summer of 2009. Its laser altimeter (LOLA) and camera (LROC) are recording the rugged, airless lunar terrain in exceptional detail, making it possible to visualize the Moon with unprecedented fidelity. This is especially evident in the long shadows cast near the terminator, or day-night line.",
-    "type": "moon"
-  }
+  // {
+  //   "name": "Pacific Ocean in Holding Pattern for El Niño",
+  //   "image": "assets/images/PIA03850_orig.jpg",
+  //   "description": "The Pacific Ocean doesnt show signs of anything that looks like the whopper El Niño of 1997-1998, according to the latest information from NASA U.S.-French ocean-observing satellite Topex/Poseidon.",
+  //   "type": "ocean"
+  // },
+  // {
+  //   "name": "Earth Science",
+  //   "image": "assets/images/0202795_medium.jpg",
+  //   "description": "This image depicts a full view of the Earth, taken by the Geostationary Operational Environment Satellite (GOES-8). The red and green charnels represent visible data, while the blue channel represents inverted 11 micron infrared data. The north and south poles were not actually observed by GOES-8. To produce this image, poles were taken from a GOES-7 image. Owned and operated by the National Oceanic and Atmospheric Administration (NOAA), GOES satellites provide the kind of continuous monitoring necessary for intensive data analysis. They circle the Earth in a geosynchronous orbit, which means they orbit the equatorial plane of the Earth at a speed matching the Earth's rotation. ",
+  //   "type": "earth"
+  // },
+  // {
+  //   "name": "Earth Observation",
+  //   "image": "assets/images/iss040e064832_large.jpg",
+  //   "description": "Earth observation taken by the Expedition 40 crew aboard the International Space Station (ISS). Folder labels this as: TL Aurora w New Zealand at end.",
+  //   "type": "earth"
+  // },
+  // {
+  //   "name": "Whirlpool Galaxy",
+  //   "image": "assets/images/PIA04230_orig.jpg",
+  //   "description": "The image from NASA Hubble Telescope shows spiral arms and dust clouds in the nearby Whirlpool galaxy. Visible starlight and light from the emission of glowing hydrogen is seen, which is associated with the most luminous young stars in the spiral arms.",
+  //   "type": "galaxy"
+  // },
+  // {
+  //   "name": "Andromeda Galaxy",
+  //   "image": "assets/images/PIA04921_medium.jpg",
+  //   "description": "This image is from NASA Galaxy Evolution Explorer is an observation of the large galaxy in Andromeda, Messier 31. The Andromeda galaxy is the most massive in the local group of galaxies that includes our Milky Way.",
+  //   "type": "galaxy"
+  // },
+  // {
+  //   "name": "Full Moon",
+  //   "image": "assets/images/GSFC_20171208_Archive_e000868_large.jpg",
+  //   "description": "Full Moon. Rises at sunset, high in the sky around midnight. Visible all night. NASA's Lunar Reconnaissance Orbiter (LRO) has been in orbit around the Moon since the summer of 2009. Its laser altimeter (LOLA) and camera (LROC) are recording the rugged, airless lunar terrain in exceptional detail, making it possible to visualize the Moon with unprecedented fidelity. This is especially evident in the long shadows cast near the terminator, or day-night line.",
+  //   "type": "moon"
+  // }
 ]
